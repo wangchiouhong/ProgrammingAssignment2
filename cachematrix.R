3f10ae1c0a3c9327c75fd08062b65f8efe940673
wangchiouhong <vic.wangchiouhong@gmail.com>
2014-05-28 11:18:37 +0800

    first assignment commit
    makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,84 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() and cacheSolve()
+## return cached inverse matrix for invertible square matrix
+## calculate the inverse matrix if no cached inverse matrix
+## E.g., x<-makeCacheMatrix(matrix(c(1,0,1,5,-3,1,2,4,7),nrow=3))
+##        cacheSolve(x)
 
-## Write a short comment describing this function
+
+# create cache for inverse matrix
+# Argument must be an invertible matrix
+# The argument should include square matrix data and number of row
+# E.g., matrix(c(1,0,1,5,-3,1,2,4,7),nrow=3)
+## makeCacheMatrix() there are API for 4 functions register to a list 
+## 1.get square invertible matrix
+## 2.set square invertible matrix from user
+## 3.get cached inverse matrix    
+## 4.set inversed matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
+    # clear the cached inversed matrix
+    inv <- NULL
+    # get()  return matrix 'x' data
+    get<-function(){
+        
+        x
+    }
+    
+    ##set() setting an matrix and clear the cached inverse matrix
+    
+    set <- function(z) {
+        x <<- z
+        inv <<- NULL
+    }
+    ## setInverseMatrix() setting cache for inverse matrix
+    
+    setInverseMatrix<-function(w){
+        
+        inv <<- w
+        
+    }
+    ## getInverseMatrix() return an iverse matrix
+    getInverseMatrix<-function() {
+        
+        inv
+    }
+    ## API interface for 
+    list(get=get, set=set,
+         getInverseMatrix = getInverseMatrix,
+         setInverseMatrix = setInverseMatrix)
+    
+    
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() to comput the inverse matrix if there is 
+## no cached inversed matrix
+## argument is the return of the makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ## get the cached inverse matrix from makeCacheMatrix()
+    i <- x$getInverseMatrix() 
+    
+    ## return the cached data if if the inverse Matrix exists
+    if (!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    ## compute the inverse matrix
+    ## get matrix data
+    data <- x$get()
+    ## compute the inverse matrix by solve()
+    ans <- solve(data)
+    
+    ## set inverse matrix to cache in makeCacheMatrix()
+    ## and display if the inverse matrix not NULL
+    if (!is.null(ans)) {
+        x$setInverseMatrix(ans)
+        ans
+    }
+    else 
+        message("inverse matrix does not exist!!")
+    
+    
 }
